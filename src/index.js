c3da74d25eb9691d25f740683476c7cf05e58a9c
RimaGai <puolus@gmail.com>
2025-10-03 19:14:52 +0200

    UX improvements

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ function showHaiku(response) {
   new Typewriter("#haiku", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 100,
+    delay: 80,
   });
 }
 
 function generateHaiku(event) {
   event.preventDefault();
 
+  let loadingElement = document.querySelector("#loading");
+  let haikuElement = document.querySelector("#haiku");
+
+  haikuElement.textContent = "";
+  loadingElement.classList.add("show");
+
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "f3474034becbfaec1efe101oeef3cft3";
   let prompt = `Generate a haiku based on ${instructionsInput.value}`;
-  let context = "You are a haiku expert. Please generate a haiku based on users input. Only display haiku itself";
+  let context = "You are a haiku expert. Only display the haiku itself.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(showHaiku);
+  axios.get(apiUrl).then((response) => {
+    loadingElement.classList.remove("show");
+    showHaiku(response);
+  });
 }
 
 let haikuFormElement = document.querySelector("#haiku-form");
